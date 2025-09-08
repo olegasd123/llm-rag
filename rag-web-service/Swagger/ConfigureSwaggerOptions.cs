405b6cfc6590b19b6a0ba3890f7777b79f2c8fcb
Oleg <oleg.verhoglyad@gmail.com>
2025-09-09 00:34:41 +0300

    Token prompt for swagger

--- rag-web-service/Swagger/ConfigureSwaggerOptions.cs
+++ rag-web-service/Swagger/ConfigureSwaggerOptions.cs
@@ -12,6 +12,36 @@ public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
 
     public void Configure(SwaggerGenOptions options)
     {
+        // Add global Bearer (JWT) security definition so Swagger UI allows tokens
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header. Enter only the token or 'Bearer {token}'.",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT"
+        });
+
+        // Apply the security requirement globally to all operations
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "bearer",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+
         foreach (var description in _provider.ApiVersionDescriptions)
         {
             var info = new OpenApiInfo
@@ -27,4 +57,3 @@ public class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
         }
     }
 }
-
